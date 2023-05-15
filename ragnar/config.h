f1cfac1b3d7a480f28318d987cdcff6ea68c3499
Cococry <cococry.btw@gmail.com>
2023-05-15 18:03:26 +0200

    Ragnar config

--- ragnar/config.h
+++ ragnar/config.h
@@ -26,7 +26,7 @@ typedef struct {
 #define APPLICATION_LAUNCHER_CMD "dmenu_run &"
 
 /* Keybindings */
-#define MASTER_KEY Mod4Mask
+#define MASTER_KEY Mod4Mask 
 
 #define TERMINAL_OPEN_KEY XK_Return
 #define WEB_BROWSER_OPEN_KEY XK_W
@@ -46,6 +46,7 @@ typedef struct {
 #define WINDOW_LAYOUT_DECREASE_SLAVE_Y_KEY XK_J
 #define WINDOW_LAYOUT_FLOATING_KEY XK_R
 #define WINDOW_LAYOUT_TILED_MASTER_KEY XK_T
+#define WINDOW_CYCLE_KEY XK_Tab
 
 #define WINDOW_GAP_INCREASE_KEY XK_plus
 #define WINDOW_GAP_DECREASE_KEY XK_minus
@@ -62,9 +63,9 @@ typedef struct {
 #define DESKTOP_COUNT 10
 
 /* Window properties */
-#define WINDOW_BG_COLOR 0x32302f
+#define WINDOW_BG_COLOR 0x32302f 
 #define WINDOW_BORDER_WIDTH 3 // In pixles
-#define WINDOW_BORDER_COLOR 0x242424
+#define WINDOW_BORDER_COLOR 0x242424 
 #define WINDOW_MIN_SIZE_Y_LAYOUT 100 // In pixels
 #define WINDOW_MAX_GAP 400 // In pixels
 
@@ -86,49 +87,53 @@ typedef struct {
 #define BAR_SHOW_INFO_LABEL true
 #define BAR_SHOW_VERSION_LABEL true
 
-#define BAR_SIZE 20 // In pixels
-#define BAR_START_MONITOR 0 // Monitor on which the bar is on. (0 is most left)
+#define BAR_SIZE 40 // In pixels
+#define BAR_START_MONITOR 1 // Monitor on which the bar is on. (0 is most left)
 #define BAR_REFRESH_SPEED 1.0 // In seconds
 #define BAR_COLOR 0x202020
 #define BAR_LABEL_PADDING 100 // In pixels
-#define BAR_FONT "JetBrains Mono Nerd Font:size=11:style=bold"
-#define BAR_FONT_SIZE 11
+                            
+#define BAR_FONT "JetBrains Mono Nerd Font:size=14:style=bold"
+#define BAR_FONT_SIZE 14
 #define BAR_FONT_COLOR "#ffffff"
 
 
-#define BAR_MAIN_LABEL_COLOR 0x1e404f
-#define BAR_INFO_LABEL_COLOR 0x1e404f
-#define BAR_BUTTON_LABEL_COLOR 0x1e404f
-#define BAR_VERSION_LABEL_COLOR 0x1e404f
-
+#define BAR_MAIN_LABEL_COLOR 0x664e43
+#define BAR_INFO_LABEL_COLOR 0x664e43 
+#define BAR_BUTTON_LABEL_COLOR 0x664e43 
+#define BAR_VERSION_LABEL_COLOR 0x664e43 // #664e43
+                            
 #define BAR_INFO_PROGRAM_ICON ""
 #define BAR_INFO_MONITOR_ICON "󰍹"
 #define BAR_INFO_DESKTOP_ICON ""
 #define BAR_INFO_WINDOW_LAYOUT_ICON ""
-#define BAR_SLICES_COUNT 7
-#define BAR_BUTTON_COUNT 3
 
-static BarCommand  BarCommands[BAR_SLICES_COUNT] =
-{
+#define BAR_SLICES_COUNT 4
+#define BAR_BUTTON_COUNT 5
+
+static BarCommand  BarCommands[BAR_SLICES_COUNT] = 
+{ 
     (BarCommand){.cmd = "echo \"  󰣇\"", .refresh_time = 300.0f},
-    (BarCommand){.cmd = "clock-xragbar", .refresh_time = 1.0f},
-    (BarCommand){.cmd = "ram-xragbar", .refresh_time = 1.0f,},
-    (BarCommand){.cmd = "kernel-xragbar", .refresh_time = 300.0f},
-    (BarCommand){.cmd = "uptime-xragbar", .refresh_time = 1.0f},
-    (BarCommand){.cmd = "packages-xragbar", .refresh_time = 60.0f},
-    (BarCommand){.cmd = "updates-xragbar", .refresh_time = 300.0f},
+    (BarCommand){.cmd = "echo \"  $(date +%R) \"", .refresh_time = 1.0f},
+    (BarCommand){.cmd = "ram-ragbar", .refresh_time = 1.0f,},
+    //(BarCommand){.cmd = "kernel-ragbar", .refresh_time = 300.0f},
+    (BarCommand){.cmd = "uptime-ragbar", .refresh_time = 1.0f},
+    //(BarCommand){.cmd = "packages-ragbar", .refresh_time = 60.0f},
+    //(BarCommand){.cmd = "updates-ragbar", .refresh_time = 120.0f},
 };
 
 static BarButton BarButtons[BAR_BUTTON_COUNT] =
 {
-    (BarButton){.cmd = APPLICATION_LAUNCHER_CMD, .icon = ""},
-    (BarButton){.cmd = TERMINAL_CMD, .icon = ""},
-    (BarButton){.cmd = WEB_BROWSER_CMD, .icon = ""},
+    (BarButton){.cmd = APPLICATION_LAUNCHER_CMD, .icon = " "},
+    (BarButton){.cmd = TERMINAL_CMD, .icon = " "},
+    (BarButton){.cmd = WEB_BROWSER_CMD, .icon = " "},
+    (BarButton){.cmd = "qtfm &", .icon = " "},
+    (BarButton){.cmd = "nitrogen &", .icon = " "},
 };
 
 /* Monitors */
 // Ordered From left to right (0 is most left)
-#define MONITOR_COUNT 1
-static const Monitor Monitors[MONITOR_COUNT] = {(Monitor){.width = 1920, .height = 1080}};
+#define MONITOR_COUNT 2
+static const Monitor Monitors[MONITOR_COUNT] = {(Monitor){.width = 1920, .height = 1080}, (Monitor){.width = 2560, .height = 1440}};
 
-static const uint32_t BarButtonLabelPos[MONITOR_COUNT] = { 1450 };
+static const uint32_t BarButtonLabelPos[MONITOR_COUNT] = { 1350, 1600 };
